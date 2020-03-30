663a504b8363bac9f3410b4946ba5f68fb5277bd
wbglaeser <ben.glaeser@tuta.io>
2020-03-30 22:05:11 +0200

    retrieve coordinate values

--- packages/analysis/bmz/location_exploration.py
+++ packages/analysis/bmz/location_exploration.py
@@ -1,2 +1,69 @@
+import os
+import re
+
 import pandas as pd
 
+from packages.toolkit.data_handler_base.data_handler_base import BaseDataHandler
+
+class BmzAnalyser(BaseDataHandler):
+
+    def __init__(self, directory: str, filename: str):
+        super().__init__(
+            input_dir="production_input",
+            output_dir="analysis_output",
+            input_filesheet="file_sheet.json",
+            output_filesheet="file_sheet.json",
+            project_directory=directory
+        )
+        self.filename = filename
+
+        # verify file structure
+        self._verify_file_structure()
+
+    def _load_data(self) -> pd.DataFrame:
+        """ load file into dataframe """
+
+        file_path = os.path.join(self.input_directory_path, self.filename)
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(f"File for analysis does not exist: {file_path}")
+
+        _, ext = os.path.splitext(file_path)
+        assert ext == ".csv", "Input file has data type which is currently not supported"
+
+        return pd.read_csv(file_path)
+
+    @staticmethod
+    def filter_relevant_fields(df: pd.DataFrame) -> pd.DataFrame:
+        """ select relevant data fields """
+
+        # select relevant fields
+        fields = ["identifier", "location_city", "location_coordinates", "budget"]
+        df = df[fields]
+
+        # setup regex
+        clutter = r"[()\']"
+
+        # split coordinates
+        df["latitude"] = df["location_coordinates"].apply(lambda x: re.sub(clutter, "", x.split(",")[0]))
+        df["longitude"] = df["location_coordinates"].apply(lambda x: re.sub(clutter, "", x.split(",")[1]))
+
+        # drop unnecessary field
+        df.drop(["location_coordinates"], axis=1, inplace=True)
+
+        return df
+
+    def run_analysis(self):
+        """ run analysis pipeline """
+
+        df = self._load_data()
+        df = self.filter_relevant_fields(df)
+        df_json = df.to_dict(orient="records")
+        self.store_data(df_json, self.filename)
+
+
+if __name__ == "__main__":
+    loc = BmzAnalyser(
+        directory="data/bmz",
+        filename="projects_25032020.csv"
+    )
+    loc.run_analysis()
--- packages/toolkit/data_handler_base/data_handler_base.py
+++ packages/toolkit/data_handler_base/data_handler_base.py
@@ -18,7 +18,6 @@ class BaseDataHandler:
         self.output_filesheet = output_filesheet
         self.project_directory = project_directory
 
-
     def _verify_file_structure(self):
         """ make sure necessary directories exist """
 
@@ -26,19 +25,19 @@ class BaseDataHandler:
             print(self.project_directory)
             raise FileNotFoundError(f"Provided data directory does not exist: {self.project_directory}")
 
-        self.input_directory = os.path.join(self.project_directory, self.input_dir)
-        if not os.path.exists(self.input_directory):
-            raise FileNotFoundError(f"Provided data directory does not exist: {self.input_directory}")
+        self.input_directory_path = os.path.join(self.project_directory, self.input_dir)
+        if not os.path.exists(self.input_directory_path):
+            raise FileNotFoundError(f"Provided data directory does not exist: {self.input_directory_path}")
 
-        self.out_directory = os.path.join(self.project_directory, self.output_dir)
-        if not os.path.exists(self.out_directory):
+        self.out_directory_path = os.path.join(self.project_directory, self.output_dir)
+        if not os.path.exists(self.out_directory_path):
             print("Target directory does not exist. Trying to create ...")
-            os.makedirs(self.out_directory)
+            os.makedirs(self.out_directory_path)
 
     def _load_file_list(self) -> list:
         """ extract list of files to be loaded """
 
-        file_sheet_path = os.path.join(self.input_directory, self.input_filesheet)
+        file_sheet_path = os.path.join(self.input_directory_path, self.input_filesheet)
         if not os.path.exists(file_sheet_path):
             raise FileNotFoundError("File-sheet does not exist")
 
@@ -46,14 +45,14 @@ class BaseDataHandler:
             file_sheet = json.load(fp)
 
         file_list = file_sheet["files"]
-        file_list_full_path = [os.path.join(self.input_directory, fname) for fname in file_list]
+        file_list_full_path = [os.path.join(self.input_directory_path, fname) for fname in file_list]
 
         return file_list_full_path
 
     def _update_filesheet(self, filename: str):
         """ update filesheet to reflect extracted files """
 
-        filesheet_path = os.path.join(self.out_directory, self.output_filesheet)
+        filesheet_path = os.path.join(self.out_directory_path, self.output_filesheet)
         if not os.path.exists(filesheet_path):
             print("Filesheet for output data does not exist currently. Creating file... ")
 
@@ -73,12 +72,12 @@ class BaseDataHandler:
     def store_data(self, records, filename: str):
         """ store extracted data in output directory """
 
-        assert type(records) in [list, pd.DataFrame], "File has unsupported type for saving."
+        assert type(records) in [list, pd.DataFrame], "Data type currently not supported for saving."
 
         # prepare file name
         file_name_short = filename.split("/")[-1]
         file_name_trunk, _ = os.path.splitext(file_name_short)
-        file_path = os.path.join(self.out_directory, file_name_trunk)
+        file_path = os.path.join(self.out_directory_path, file_name_trunk)
 
         if isinstance(records, list):
             file_path_ext = file_path + ".json"
