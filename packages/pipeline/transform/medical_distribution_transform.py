4f9cefd14b3bc46eb86d97eeed14ac94681e4314
Maan Al Balkhi <maan.al-balkhi@aperto.com>
2020-04-16 12:33:09 +0200

    transformer for medical and states population

---
+++ packages/pipeline/transform/medical_distribution_transform.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+
+class MedicalDistributionTransformer:
+    """ Class to extract relevant data from demographics data """
+
+    tag = "medical_distribution"
+
+    def __init__(self, file_name):
+
+        self.file_name = file_name
+
+    def load_data(self) -> pd.DataFrame:
+        """ load and parse xml file """
+
+        if not os.path.exists(self.file_name):
+            raise FileNotFoundError(f"File does not exist: {self.file_name}")
+
+        _, ext = os.path.splitext(self.file_name)
+        assert ext == ".csv", "Invalid filetype attempted to load"
+
+        return pd.read_csv(self.file_name)
+
+    def transform_document(self) -> pd.DataFrame:
+        """ Parse entire file """
+
+        return self.load_data()
+
+    @classmethod
+    def pipe(cls, filename: str) -> pd.DataFrame:
+        """ run entire transform pipeline """
+
+        transformer = cls(filename)
+        data = transformer.transform_document()
+
+        return data
